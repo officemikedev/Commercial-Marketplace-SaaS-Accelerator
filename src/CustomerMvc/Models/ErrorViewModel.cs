e92db12175b06f083aefa918fc48fdb845cf9856
office mike <mike@officemike.com>
2024-12-23 12:32:19 -0600

    CustomerMvc2 deployed starts up with auth

---
+++ src/CustomerMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CustomerMvc.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ src/CustomerMvc2/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CustomerMvc2.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
